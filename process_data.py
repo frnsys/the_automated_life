42587fdefb11218cfbbdf4bd3f3aff195c37cc1c
Francis Tseng <f+accounts@frnsys.com>
2019-02-15 10:27:48 -0500

    use network data from Skillscape

--- process_data.py
+++ process_data.py
@@ -10,26 +10,48 @@ jobs = {}
 skills = {}
 
 MIN_SKILLS = 7
-MIN_SKILL_WEIGHT = 1.0
+MIN_SKILL_WEIGHT = 1.5
+
+# Job node positions for network layout
+job_network_layout = {}
+data = json.load(open('data/src/jobNetwork.json'))
+for job in data['nodes']:
+    job_network_layout[job['id']] = {
+        'x': job['x'],
+        'y': job['y']
+    }
 
 # Job-skill matrix
 df = pd.read_csv('data/src/jobSkillRcaMat.csv', delimiter='\t')
 n_jobs = len(df)
 n_skills = []
 skills = {i: name.strip() for i, name in enumerate(df.columns.tolist()[2:])}
+skill_weights = []
 for i, r in tqdm(df.iterrows()):
     vals = r.tolist()[2:]
     job_skills = {}
     for j, v in enumerate(vals):
+        skill_weights.append(v)
         if v >= MIN_SKILL_WEIGHT: job_skills[j] = v
+    id = r['Job Code']
+    try:
+        job_network_layout[id]
+    except KeyError:
+        print('missing:', id, r[' Job Title'])
+        continue
     jobs[i] = {
         'name': r[' Job Title'].strip(),
-        'skills': job_skills
+        'skills': job_skills,
+        'pos': job_network_layout[id]
     }
     n_skills.append(len(job_skills))
     # print('n skills:', len(job_skills))
     assert len(job_skills) >= MIN_SKILLS
 print('Mean skills:', np.mean(n_skills))
+# print('Mean skill weight:', np.mean(skill_weights))
+# print('Min skill weight:', np.min(skill_weights))
+# print('Max skill weight:', np.max(skill_weights))
+# print('Percentile skill weight:', np.percentile(skill_weights, 80))
 
 jobs_inv = {j['name']: i for i, j in jobs.items()}
 skills_inv = {name: i for i, name in skills.items()}
@@ -47,16 +69,19 @@ for i, r in tqdm(skill_skill.iterrows()):
 df = pd.read_csv('data/src/jobJobSkillSims.tsv', delimiter='\t')
 job_job = np.zeros((n_jobs, n_jobs))
 for i, r in tqdm(df.iterrows()):
-    a = jobs_inv[r['Title 1']]
-    b = jobs_inv[r['Title 2']]
-    job_job[a][b] = job_job[b][a] = r['Skill Sim']
+    try:
+        a = jobs_inv[r['Title 1']]
+        b = jobs_inv[r['Title 2']]
+        job_job[a][b] = job_job[b][a] = r['Skill Sim']
+    except KeyError:
+        # skipped job
+        continue
 
-max_neighbors = 6
-min_neighbors = 3 # min so that the graph is connected
-min_similarity = 0.6
+min_neighbors = 1 # min so that the graph is connected
+min_similarity = 0.5
 for idx, job in jobs.items():
     min_sim = min_similarity
-    cands = np.argsort(job_job[idx])[-max_neighbors:]
+    cands = np.argsort(job_job[idx])
     similar = [id for id in cands if job_job[idx][id] >= min_sim]
 
     # if no similar, incrementally reduce minimum similarity
