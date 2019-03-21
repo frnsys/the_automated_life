7667d92ab850e78223813a697353535489d3d36d
Francis Tseng <f+accounts@frnsys.com>
2019-03-21 11:58:48 -0400

    show which skills are improving from current job

--- app/ui/skills.js
+++ app/ui/skills.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Bar, BarFill } from './styles'
 import skills from 'data/skills.json'
+import logic from '../logic';
 
 const SkillsStyle = styled('div')`
   max-height: 80vh;
@@ -16,13 +17,16 @@ const SkillsStyle = styled('div')`
 
 class Skills extends Component {
   render() {
+    let skillChanges = logic.workSkillGain(this.props.job, 1);
+    let changingSkills = Object.keys(skillChanges);
+
     return <SkillsStyle>
       <h3>Your Skills</h3>
       <table>
         <tbody>
           {Object.keys(this.props.skills).map((s_id) => {
             return <tr key={s_id}>
-              <td>{skills[s_id].name}</td>
+              <td>{changingSkills.includes(s_id) ? <img title="Improving on this job" alt="Improving on this job" src="/arrow.png" style={{width: '12px'}} /> : ''} {skills[s_id].name}</td>
               <td><Bar style={{width: '3em', height: '1.5em', background: 'none'}}><BarFill style={{width: `${this.props.skills[s_id]*100}%`}} /></Bar></td></tr>;
           })}
         </tbody>
@@ -33,6 +37,7 @@ class Skills extends Component {
 
 const mapStateToProps = (state, props) => {
   return {
+    job: state.player.job,
     skills: state.player.skills
   };
 };
