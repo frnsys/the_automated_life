e6acf579edb5111eccd302e1a9ac838fa431454f
Francis Tseng <f+accounts@frnsys.com>
2019-04-02 20:18:28 -0400

    enable logging config option

--- app/log.js
+++ app/log.js
@@ -1,21 +1,23 @@
+import config from 'config';
 import uuid from 'uuid/v4';
 
 const IDENTIFIER = uuid();
 
 const log = (data) => {
-  fetch('/log', {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      id: IDENTIFIER,
-      ev: data,
-      ts: Date.now()
-    })
-  })
-    .catch(err => { throw err });
+  if (config.enableLogging) {
+    fetch('/log', {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        id: IDENTIFIER,
+        ev: data,
+        ts: Date.now()
+      })
+    }).catch(err => { throw err });
+  }
 }
 
 export default log;
