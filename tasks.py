c1b610ce40d8747261be9b83c31381773075b0f2
Francis Tseng <f+accounts@frnsys.com>
2019-04-17 14:00:12 -0400

    integrating celery for periodic stat aggregation

---
+++ tasks.py
@@ -0,0 +1,41 @@
+import json
+import redis
+from celery import Celery
+from celery.schedules import crontab
+from collections import defaultdict
+
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+CELERY_RESULT_BACKEND = None
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+CELERY_RESULT_BACKEND = None
+CELERYBEAT_SCHEDULE = {
+    'aggregate_statistics': {
+        'task': 'tasks.aggregate_statistics',
+        'schedule': crontab(minute=0, hour=0),
+    }
+}
+
+redis = redis.Redis(host='localhost', port=6379, db=1)
+celery = Celery('tasks', backend=CELERY_RESULT_BACKEND, broker=CELERY_BROKER_URL)
+celery.conf.beat_schedule = CELERYBEAT_SCHEDULE
+
+@celery.task
+def aggregate_statistics(schedule):
+    # Load summaries
+    keys = [r.decode('utf8') for r in redis.keys('fow:*:summary')]
+    aggs = defaultdict(defaultdict(int))
+    for k in keys:
+        res = redis.get(k)
+        data = json.loads(res)
+        aggs['n_jobs']['sum'] += len(data['jobs'])
+        aggs['n_jobs']['total'] += 1
+        aggs['wins']['sum'] += int(data['success'])
+        aggs['wins']['total'] += 1
+        aggs['n_applied']['sum'] += len(data['nApplied'])
+        aggs['n_applied']['total'] += 1
+        aggs['n_rejected']['sum'] += len(data['nRejected'])
+        aggs['n_rejected']['total'] += 1
+
+    # Compute aggregate stats
+    res = redis.set('fow:aggregate', json.dumps(aggs))
+    redis.save()
