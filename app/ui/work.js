399b546cfeb8575a25e73593170ac19ab92455eb
Francis Tseng <f+accounts@frnsys.com>
2019-10-16 14:49:26 -0400

    work pattern game

--- app/ui/work.js
+++ app/ui/work.js
@@ -1,9 +1,53 @@
 import t from 'i18n';
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import config from 'config';
 
+function checkTask(taskType, taskIdx, seq, pattern, setSeq, doWork, doTask) {
+  if (window.paused) {
+    return;
+  } else {
+    // Update current task sequence
+    let curSeq = seq.concat([taskType]);
+
+    // Wrong pattern
+    if (!curSeq.every((t, i) => t == undefined || t == pattern[i])) {
+      curSeq = [];
+
+      // Flash red
+      let barOutline = document.querySelector('.work-bar');
+      barOutline.classList.add('bar-failure');
+      setTimeout(() => {
+        barOutline.classList.remove('bar-failure');
+      }, 300);
+
+
+    // Correct pattern
+    } else if (pattern.every((t, i) => t == curSeq[i])) {
+      curSeq = [];
+
+      // Flash green
+      let barOutline = document.querySelector('.work-bar');
+      barOutline.classList.add('bar-success');
+      setTimeout(() => {
+        barOutline.classList.remove('bar-success');
+      }, 300);
+
+      // Successful work
+      doWork();
+    }
+
+    // Always remove the task
+    doTask(taskIdx);
+
+    // Update state
+    setSeq(curSeq);
+  }
+}
+
 const Work = (props) => {
+  const [seq, setSeq] = useState([]);
+
   // Don't show work minigame if not employed
   let job = props.player.job.name;
   if (job == 'Unemployed' || job == 'Student') {
@@ -28,7 +72,16 @@ const Work = (props) => {
     performance = t('performance_fantastic');
     performanceColor = '#d119e5';
   }
+
+  let pattern = [0,1,1,0];
+  // let pattern = props.player.job.pattern; // TODO
+
   return <div className='work'>
+    <div class='work-pattern'>
+      {pattern.map((taskType, i) => {
+        return <div className='work-pattern-icon' key={i} style={{backgroundColor: config.taskColors[taskType]}}>{seq[i] == taskType ? '✓' : ''}</div>;
+      })}
+    </div>
     <div className='work-bar'>
       <div className='bar' style={{background: '#fff'}}>
         <div className='bar-fill' style={{width: `${props.player.performance}%`}} />
@@ -36,8 +89,8 @@ const Work = (props) => {
       <div className='performance-label'>{t('performance')}: <span style={{color: performanceColor}}>{performance}</span></div>
     </div>
     <div>
-      {[...Array(props.player.tasks)].map((i) => {
-        return <div className='work-button' key={i} onClick={() => window.paused ? '' : props.work()}>{t('work_button')}</div>;
+      {props.player.tasks.map((taskType, i) => {
+        return <div className='work-button' style={{backgroundColor: config.taskColors[taskType]}} key={i} onClick={() => checkTask(taskType, i, seq, pattern, setSeq, props.work, props.task)}>{t('work_button')}</div>;
       })}
     </div>
   </div>;
@@ -51,6 +104,12 @@ const mapActionsToProps = {
   work: () => {
     return {
       type: 'player:work'
+    };
+  },
+  task: (taskIndex) => {
+    return {
+      type: 'player:task',
+			payload: taskIndex
     };
   }
 }
