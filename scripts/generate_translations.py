a9f31575b4e7c495daa75cbe764eb1a283866ff5
Francis Tseng <f+accounts@frnsys.com>
2020-08-27 11:01:54 -0400

    added tutorial translations

--- scripts/generate_translations.py
+++ scripts/generate_translations.py
@@ -32,3 +32,38 @@ for lang in LANGS:
 
     with open('../static/lang/{}.json'.format(lang), 'w') as f:
         json.dump(data, f, sort_keys=True, indent=4)
+
+    translations = data
+
+    changes = 0
+    csv = pd.read_csv('translations/updates/{}.csv'.format(lang))
+    original_col = csv.columns[1]
+    correction_col = csv.columns[-1]
+    for i, row in csv.iterrows():
+        if isinstance(row[correction_col], str):
+            original = row[original_col]
+            translations[original] = row[correction_col]
+            changes += 1
+    print(lang, changes)
+
+    with open('../static/lang/{}.json'.format(lang), 'w') as f:
+        json.dump(translations, f, sort_keys=True, indent=4)
+
+
+df = pd.read_csv('translations/src/tutorial.csv')
+TRANS = {lang: {} for lang in LANGS}
+for i, row in df.iterrows():
+    key = row['key']
+    for lang in LANGS:
+        trans = row['{} corrections'.format(lang)]
+        TRANS[lang][key] = trans
+
+for lang in LANGS:
+    with open('../static/lang/{}.json'.format(lang), 'r') as f:
+        translations = json.load(f)
+
+    for key, trans in TRANS[lang].items():
+        translations[key] = trans
+
+    with open('../static/lang/{}.json'.format(lang), 'w') as f:
+        json.dump(translations, f, sort_keys=True, indent=4)
