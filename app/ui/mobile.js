6643ec97dd9b18b5747b0a8793e40f631b6cfa10
Francis Tseng <f+accounts@frnsys.com>
2021-02-23 10:00:44 -0500

    mobile warning component

---
+++ app/ui/mobile.js
@@ -0,0 +1,12 @@
+import t from 'i18n';
+import React, { Component } from 'react';
+
+const MobileWarning = () => {
+  return <div className="mobile-warning">
+    <div>
+      {t('mobile_warning')}
+    </div>
+  </div>
+};
+
+export default MobileWarning;
